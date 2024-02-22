2c085e4e6d70052437be47007813af190dd42dc9
Liyeuk Reynald <reynaldjoabet@gmail.com>
2024-02-22 13:51:22 +0400

    more changes to application configuration

--- src/main/scala/configs/RedisConfiguration.scala
+++ src/main/scala/configs/RedisConfiguration.scala
@@ -1,9 +1,15 @@
 package configs
-
-object RedisConfiguration {}
+import pureconfig.ConfigReader
+import pureconfig.generic.semiauto.deriveReader
 case class RedisConfiguration(
     host: String,
     port: Int,
     password: String,
     database: Int
 )
+
+object RedisConfiguration {
+
+  implicit val redisConfigReader: ConfigReader[RedisConfiguration] =
+    deriveReader[RedisConfiguration]
+}
---
+++ src/main/scala/configs/StripeConfig.scala
@@ -0,0 +1,17 @@
+package configs
+
+import pureconfig.ConfigReader
+import pureconfig.generic.semiauto.deriveReader
+
+final case class StripeConfig(
+    key: String,
+    price: String,
+    successUrl: String,
+    cancelUrl: String,
+    webhookSecret: String
+)
+
+object StripeConfig {
+  implicit val stripeConfigReader: ConfigReader[StripeConfig] =
+    deriveReader[StripeConfig]
+}
